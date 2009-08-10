da4810d2db24bdebc90825ca8fd7f9012e010a75
Jason Newton <nevion@gmail.com>
2009-08-09 20:25:56 -0700

    added signal callbacks

--- qpaeq.py
+++ qpaeq.py
@@ -1,7 +1,11 @@
-import dbus,os,math,sys
+import os,math,sys
 import PyQt4
 from PyQt4 import QtGui,QtCore
 from functools import partial
+
+import dbus.mainloop.qt
+import dbus
+
 
 CORE_PATH = "/org/pulseaudio/core1"
 CORE_IFACE = "org.PulseAudio.Core1"
@@ -10,8 +14,8 @@ def connect():
         address = os.environ['PULSE_DBUS_SERVER']
     else:
         bus = dbus.SessionBus() # Should be UserBus, but D-Bus doesn't implement that yet.
-        server_lookup = bus.get_object('org.PulseAudio1', "/org/pulseaudio/server_lookup1")
-        address = server_lookup.Get("org.PulseAudio.ServerLookup1", "Address", dbus_interface="org.freedesktop.DBus.Properties")
+        server_lookup = bus.get_object('org.PulseAudio1', '/org/pulseaudio/server_lookup1')
+        address = server_lookup.Get('org.PulseAudio.ServerLookup1', 'Address', dbus_interface='org.freedesktop.DBus.Properties')
     return dbus.connection.Connection(address)
 
 
@@ -24,30 +28,17 @@ def hz2str(hz):
         return '%dHz' %(hz,)
     elif p>=3:
         return '%.1fKHz' %(hz/(10.0**3),)
-#values = the destination array 
-#points = list of tuples of frequency and coefficient
-def interpolate(values,points):
-    #Interpolate the specified frequency band values
-    #assumes the final point is a dummy default for everything past
-    #the real points
-    i,j=1,0
-    while i<len(values):
-        #if this is the last valid interpolation, fill out the rest 
-        #with the final value
-        if j==len(points)-2:
-            values[i:]=(len(values)-i)*[(points[j+1][1])]
-            break
-        #bilinear-inerpolation of coefficients specified
-        c0=(i-points[j][0])/(points[j+1][0]-points[j][0])
-        values[i]=(1.0-c0)*points[j][1]+c0*points[j+1][1]
-        while i>=math.floor(points[j+1][0]):
-            j+=1
-        i+=1
-
+#TODO: signals: sink Filter changed, sink reconfigured (window size) (sink iface)
+#TODO: manager signals: new sink, removed sink, new profile, removed profile
+#TODO: add support for changing of window_size 1000-fft_size (adv option)
+#TODO: reconnect support loop 1 second trying to reconnect
+#TODO: just resample the filters for profiles when loading to different sizes
 class QPaeq(QtGui.QWidget):
     DEFAULT_FREQUENCIES=map(float,[50,100,200,300,400,500,800,1e3,1.5e3,3e3,5e3,7e3,10e3,15e3,20e3])
     sink_iface='org.PulseAudio.Ext.Equalizing1.Equalizer'
+    manager_path='/org/pulseaudio/equalizing1' 
     manager_iface='org.PulseAudio.Ext.Equalizing1.Manager'
+    prop_iface='org.freedesktop.DBus.Properties'
     def __init__(self):
         QtGui.QWidget.__init__(self)
         self.setWindowTitle('qpaeq')
@@ -62,7 +53,7 @@ class QPaeq(QtGui.QWidget):
         slider_layout=self.create_slider_layout()
         layout=QtGui.QVBoxLayout(self)
         
-        top_layout=QtGui.QHBoxLayout()
+        toprow_layout=QtGui.QHBoxLayout()
         self.profile_box = QtGui.QComboBox()
         self.sink_box = QtGui.QComboBox()
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
@@ -70,24 +61,80 @@ class QPaeq(QtGui.QWidget):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.profile_box.sizePolicy().hasHeightForWidth())
         self.profile_box.setSizePolicy(sizePolicy)
+        self.profile_box.setDuplicatesEnabled(False)
         self.sink_box.setSizePolicy(sizePolicy)
-        top_layout.addWidget(self.sink_box)
-        top_layout.addWidget(QtGui.QLabel('Profile'))
-        top_layout.addWidget(self.profile_box)
+        self.sink_box.setDuplicatesEnabled(False)
+        toprow_layout.addWidget(QtGui.QLabel('Sink'))
+        toprow_layout.addWidget(self.sink_box)
+        toprow_layout.addWidget(QtGui.QLabel('Profile'))
+        toprow_layout.addWidget(self.profile_box)
+        large_icon_size=self.style().pixelMetric(QtGui.QStyle.PM_LargeIconSize)
+        large_icon_size=QtCore.QSize(large_icon_size,large_icon_size)
+        save_profile=QtGui.QToolButton()
+        save_profile.setIcon(self.style().standardIcon(QtGui.QStyle.SP_DriveFDIcon))
+        save_profile.setIconSize(large_icon_size)
+        save_profile.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)
+        save_profile.clicked.connect(self.save_profile)
+        remove_profile=QtGui.QToolButton()
+        remove_profile.setIcon(self.style().standardIcon(QtGui.QStyle.SP_TrashIcon))
+        remove_profile.setIconSize(large_icon_size)
+        remove_profile.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)
+        remove_profile.clicked.connect(self.remove_profile)
+        toprow_layout.addWidget(save_profile)
+        toprow_layout.addWidget(remove_profile)
+
         reset_button = QtGui.QPushButton('Reset')
         reset_button.clicked.connect(self.reset)
-        top_layout.addStretch()
-        top_layout.addWidget(reset_button)
+        toprow_layout.addStretch()
+        toprow_layout.addWidget(reset_button)
 
-        layout.addLayout(top_layout)
+        layout.addLayout(toprow_layout)
         layout.addLayout(slider_layout)
         #self.setLayout(layout)
         self.read_filter()
         self.update_profiles()
         self.update_sinks()
-
+        self.set_manager_dbus_sig_handlers()
+        self.set_sink_dbus_sig_handlers()
+    def set_manager_dbus_sig_handlers(self):
+        manager=dbus.Interface(self.manager_obj,dbus_interface=self.manager_iface)
+        manager.connect_to_signal(None,self.update_profiles)
+        manager.connect_to_signal('SinkAdded',self.sink_added)
+        manager.connect_to_signal('SinkRemoved',self.sink_removed)
+    def set_sink_dbus_sig_handlers(self):
+        self.sink.connect_to_signal('FilterChanged',self.read_filter)
+    def sink_added(self,sink):
+        self.sinks.append(sink)
+        self.update_sinks()
+    def sink_removed(self,sink):
+        if sink==self.sink_name:
+            #connect to new sink?
+            pass
+        self.update_sinks()
+        
+    def save_profile(self):
+        #popup dialog box for name
+        current=self.profile_box.currentIndex()
+        profile,ok=QtGui.QInputDialog.getItem(self,'Preset Name','Preset',self.profiles,current)
+        if not ok or profile=='':
+            return
+        if profile in self.profiles:
+            mbox=QtGui.QMessageBox(self)
+            mbox.setText('%s preset already exists'%(profile,))
+            mbox.setInformativeText('Do you want to save over it?')
+            mbox.setStandardButtons(mbox.Save|mbox.Discard|mbox.Cancel)
+            mbox.setDefaultButton(mbox.Save)
+            ret=mbox.exec_()
+            if ret!=mbox.Save:
+                return
+        self.sink.SaveProfile(dbus.String(profile))
+    def remove_profile(self):
+        #find active profile name, remove it
+        profile=self.profile_box.currentText()
+        manager=dbus.Interface(self.manager_obj,dbus_interface=self.manager_iface)
+        manager.RemoveProfile(dbus.String(profile))
     def set_frequencies_values(self,freqs):
-        self.frequencies=[0]+freqs+[self.sample_rate//2]
+        self.frequencies=freqs+[self.sample_rate//2]
         self.filter_frequencies=map(lambda x: int(round(x)), \
                 translate_rates(self.filter_rate,self.sample_rate,
                     self.frequencies) \
@@ -107,18 +154,24 @@ class QPaeq(QtGui.QWidget):
     def coef2slider(x):
         return int((x-1)*1000)
     def create_slider(self,changed_cb,index,hz):
-        slider_layout=QtGui.QVBoxLayout()
-        slider=QtGui.QSlider(self.orientation)
-        slider.setRange(-1000,1500)
-        slider.setSingleStep(1)
-        slider.valueChanged.connect(changed_cb)
         if hz==0:
             label_text='DC'
         elif hz==self.sample_rate//2:
             label_text='Coda'
         else:
             label_text=hz2str(hz)
-        slider_label=QtGui.QLabel(label_text)
+        class HzLabel(QtGui.QLabel):
+            def __init__(self,slider,label,parent=None):
+                QtGui.QLabel.__init__(self,label, parent)
+                self.slider=slider
+            def mouseDoubleClickEvent(self, event):
+                    self.slider.setValue(0)
+        slider_layout=QtGui.QVBoxLayout()
+        slider=QtGui.QSlider(self.orientation)
+        slider.setRange(-1000,2000)
+        slider.setSingleStep(1)
+        slider.valueChanged.connect(changed_cb)
+        slider_label=HzLabel(slider,label_text)
         slider_layout.addWidget(slider)
         slider_layout.addWidget(slider_label)
         self.slider[index]=slider
@@ -127,36 +180,34 @@ class QPaeq(QtGui.QWidget):
     def update_coefficient(self,i,hz,v):
         self.coefficients[i]=self.slider2coef(v)
         self.set_filter()
-    def calculate_filter(self):
-        interpolate(self.filter,zip(self.filter_frequencies,self.coefficients))
     def set_filter(self):
-        self.sink.SeedFilter(self.filter_frequencies,self.coefficients)
+        freqs=[0]+self.filter_frequencies
+        coefs=[1.0]+self.coefficients
+        self.sink.SeedFilter(freqs,coefs)
     def get_eq_attr(self,attr):
         return self.sink_props.Get(self.sink_iface,attr)
     def set_connection(self):
         self.connection=connect()
+        self.manager_obj=self.connection.get_object(object_path=self.manager_path)
     def set_managed_info(self):
-        manager_obj=self.connection.get_object(object_path='/org/pulseaudio/equalizing1')
-        manager_props=dbus.Interface(manager_obj,dbus_interface='org.freedesktop.DBus.Properties')
-        self.profiles=manager_props.Get(self.manager_iface,'Profiles')
-        #print self.profiles
+        manager_props=dbus.Interface(self.manager_obj,dbus_interface=self.prop_iface)
         self.sinks=manager_props.Get(self.manager_iface,'EqualizedSinks')
-        
     def update_profiles(self):
+        print 'update profiles called!'
+        manager_props=dbus.Interface(self.manager_obj,dbus_interface=self.prop_iface)
+        self.profiles=manager_props.Get(self.manager_iface,'Profiles')
         self.profile_box.clear()
         self.profile_box.addItems(self.profiles)
     def update_sinks(self):
         self.sink_box.clear()
         self.sink_box.addItems(self.sinks)
     def set_sink_info(self):
-        self.sink_name='/org/pulseaudio/core1/sink1'
         sink=self.connection.get_object(object_path=self.sink_name)
-        self.sink_props=dbus.Interface(sink,dbus_interface='org.freedesktop.DBus.Properties')
-        self.sink=dbus.Interface(sink,dbus_interface='org.PulseAudio.Ext.Equalizing1.Equalizer')
+        self.sink_props=dbus.Interface(sink,dbus_interface=self.prop_iface)
+        self.sink=dbus.Interface(sink,dbus_interface=self.sink_iface)
         self.sample_rate=self.get_eq_attr('SampleRate')
         self.filter_rate=self.get_eq_attr('FilterSampleRate')
     def read_filter(self):
-        #self.filter=self.get_eq_attr('FilterCoefficients')
         self.coefficients=self.sink.FilterAtPoints(self.filter_frequencies)
         for i,hz in enumerate(self.filter_frequencies):
             self.slider[i].blockSignals(True)
@@ -168,10 +219,10 @@ class QPaeq(QtGui.QWidget):
             self.coefficients[i]=1
             slider.setValue(self.coef2slider(self.coefficients[i]))
             slider.blockSignals(False)
-        #self.calculate_filter()
         self.set_filter()
         
 def main():
+    dbus.mainloop.qt.DBusQtMainLoop(set_as_default=True)
     app=QtGui.QApplication(sys.argv)
     qpaeq_main=QPaeq()
     qpaeq_main.show()
