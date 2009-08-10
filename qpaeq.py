ecdc04949f7cdacae261fe616984f85dfbb63f4d
Jason Newton <nevion@gmail.com>
2009-08-10 01:06:43 -0700

    use proper ListenForSignal calls

--- qpaeq.py
+++ qpaeq.py
@@ -110,12 +110,18 @@ class QPaeq(QtGui.QWidget):
         manager=dbus.Interface(self.manager_obj,dbus_interface=self.manager_iface)
         #self._get_core().ListenForSignal(self.manager_iface,[])
         #self._get_core().ListenForSignal(self.manager_iface,[dbus.ObjectPath(self.manager_path)])
+        core=self._get_core()
+        for x in ['ProfilesChanged','SinkAdded','SinkRemoved']:
+            core.ListenForSignal("%s.%s" %(self.manager_iface,x),[dbus.ObjectPath(self.manager_path)])
         manager.connect_to_signal('ProfilesChanged',self.update_profiles)
         manager.connect_to_signal('SinkAdded',self.sink_added)
         manager.connect_to_signal('SinkRemoved',self.sink_removed)
     def set_sink_dbus_sig_handlers(self):
-        self._get_core().ListenForSignal('',[dbus.ObjectPath(self.sink_name),dbus.ObjectPath(self.manager_path)])
-        #self._get_core().ListenForSignal(self.sink_iface,[])
+        #self._get_core().ListenForSignal('',[dbus.ObjectPath(self.sink_name),dbus.ObjectPath(self.manager_path)])
+        core=self._get_core()
+        for x in ['FilterChanged']:
+            core.ListenForSignal("%s.%s" %(self.sink_iface,x),[dbus.ObjectPath(self.sink_name)])
+        #self._get_core().ListenForSignal(self.sink_iface,[dbus.ObjectPath(self.sink_name)])
         #self._get_core().ListenForSignal(self.sink_iface,[dbus.ObjectPath(self.sink_name)])
         self.sink.connect_to_signal('FilterChanged',self.read_filter)
     def sink_added(self,sink):
